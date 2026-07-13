a2823a3365b6a43f1da7ad5553f54bab146b9e00
nexus1877 <nexusdev77@gmail.com>
2026-07-13 23:13:50 +0300

    Add configuration settings in config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+return {
+    enableEnvLog = true,
+    enableDecrypt = true,
+    enableGarbageFilter = true,
+    maxStringLength = 1000,
+    outputFile = "deobf_output.json",
+    verbose = false
+}
